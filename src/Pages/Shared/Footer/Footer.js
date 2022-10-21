181de197d76d6f2785af74c8e160d546f217b4f9
rahul-sarker18 <rahulsarker1495@gmail.com>
2022-10-22 01:12:01 +0530

    footer add

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <p><small>Copyright coming soon</small></p>
+            <p className='bg-success p-4 text-center mt-5 '><small>Copyright@ coming soon............</small></p>
         </div>
     );
 };
